74816f4f1bb501c864399ba7e1deb147a046d5e8
AnkanaPanja <ankanapanja308@gmail.com>
2024-11-24 16:20:37 +0530

    Login&Signup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.google.gms.google.services)
+
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -34,11 +35,14 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
